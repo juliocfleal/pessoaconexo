c6104689dd96b1fdd2b1223bdd7555b9df69ca55
Julio Leal <julio.leal@alunos.ifsuldeminas.edu.br>
2021-11-11 19:53:41 -0300

    Projeto criado

---
+++ src/main/java/com/julioleal/pessoaconexo/PessoaconexoApplication.java
@@ -0,0 +1,13 @@
+package com.julioleal.pessoaconexo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PessoaconexoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PessoaconexoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/julioleal/pessoaconexo/PessoaconexoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.julioleal.pessoaconexo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PessoaconexoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

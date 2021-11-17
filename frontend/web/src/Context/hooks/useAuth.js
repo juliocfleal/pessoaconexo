647c0e58360b8f634e08a9fe1a380623b47f6636
Julio Leal <julio.leal@alunos.ifsuldeminas.edu.br>
2021-11-17 00:49:46 -0300

    Correcao dos outros commits

---
+++ frontend/web/src/Context/hooks/useAuth.js
@@ -0,0 +1,37 @@
+import { useState, useEffect } from 'react';
+
+import api from '../../services/api';
+import history from '../../history';
+
+export default function useAuth() {
+  const [authenticated, setAuthenticated] = useState(false);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const token = localStorage.getItem('token');
+
+    if (token) {
+      api.defaults.headers.Authorization = `Bearer ${JSON.parse(token)}`;
+      setAuthenticated(true);
+    }
+
+    setLoading(false);
+  }, []);
+  
+  async function handleLogin() {
+    const { data: { token } } = await api.post('/login/');
+
+    localStorage.setItem('token', JSON.stringify(token));
+    api.defaults.headers.Authorization = `Bearer ${token}`;
+    setAuthenticated(true);
+  }
+
+  function handleLogout() {
+    setAuthenticated(false);
+    localStorage.removeItem('token');
+    api.defaults.headers.Authorization = undefined;
+   
+  }
+  
+  return { authenticated, loading, handleLogin, handleLogout };
+}
